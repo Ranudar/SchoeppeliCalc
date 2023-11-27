8008139ffa4e0c8aa1131964a0a8e6af5b3cd30e
Christian Epple <57997229+Ranudar@users.noreply.github.com>
2023-11-27 23:16:04 +0100

    Add initial files.

---
+++ main.js
@@ -0,0 +1,30 @@
+// https://dairyprocessinghandbook.tetrapak.com/de/chapter/waermetauscher
+// Dichte Milch = 1020 kg/m^3; Spezifische Wärmekapazität Milch: 3.95 kJ/kg
+// 1 m^3 = 1000 Liter
+
+const heatCapacityMilk = 3.95 // kJ/(kg*K)
+const relativeDensityMilk = 1.020 // kg/l
+
+function calculateMicrowaveTime(volume=220, watt=800, initialTemperature=7, targetTemperature=45) {
+  // volume in ml, temperatures degrees celsius
+  const microwaveEfficiency = 0.75
+  watt = watt * microwaveEfficiency;
+  const temperatureDifference = targetTemperature - initialTemperature; // difference celsius corresponds to kelvin
+  const mass = (volume / 1000) * relativeDensityMilk;
+  const kiloJoule = mass * temperatureDifference * heatCapacityMilk;
+  const Joule = kiloJoule * 1000;
+  const time = Joule / watt
+  return Math.round(time);
+}
+// console.log(calculateMicrowaveTime(120))
+
+const result = document.querySelector("#result");
+// const volume = document.querySelector("#volume");
+
+
+const submitButton = document.querySelector("button");
+submitButton.addEventListener("click", event => {
+  event.preventDefault();
+  result.textContent = calculateMicrowaveTime(volume.value)
+})
+
